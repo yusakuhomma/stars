8d1d467587c66248e22aab0adce74b19ac9c5c0e
yhomma <homma.yusaku@imagica.jp>
2018-08-27 06:16:41 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Department.create(name: '社長',display: 1)
+Department.create(name: '取締役',display: 2)
+Department.create(name: '映像事業本部長',display: 3)
+Department.create(name: '開発本部/技術開発室/R&Dグループ',display: 100)
+
+
+Category.create(name: '外国語',display: 10)
+Category.create(name: 'プログラミング言語',display: 30)
